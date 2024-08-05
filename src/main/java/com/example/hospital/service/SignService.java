6fd25bc0ecd9b5145c0b8258a1ce6e1bceab6ae4
baby <gkje123@naver.com>
2024-08-05 14:57:14 +0900

    refactor: 멤버 entity service 내부에서 찾도록 변경
    - 기존: 매 기능 마다 header 로 넘어온 token 직접 파싱하여 사용
    - 변경: 각 서비스 내부에서 Authentication 을 통해 멤버 받아 사용

--- src/main/java/com/example/hospital/service/SignService.java
+++ src/main/java/com/example/hospital/service/SignService.java
@@ -11,7 +11,8 @@ import com.example.hospital.exception.CustomException;
 import com.example.hospital.exception.ErrorCode;
 import com.example.hospital.repository.member.DoctorRepository;
 import com.example.hospital.repository.member.PatientRepository;
-import com.example.hospital.security.SecurityManager;
+import com.example.hospital.security.JwtProvider;
+import com.example.hospital.type.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,71 +21,78 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class SignService {
 
-  private final SecurityManager securityManager;
+  private final JwtProvider jwtProvider;
   private final DoctorRepository doctorRepository;
   private final PatientRepository patientRepository;
 
   @Transactional
   public DoctorResponse doctorSignUp(DoctorForm form) {
+
+    //아이디 중복 체크
     if (doctorRepository.existsByLoginId(form.getLoginId())) {
       throw new CustomException(ErrorCode.ID_EXIST);
     }
 
     Doctor doctor = Doctor.builder()
         .loginId(form.getLoginId())
+        .password(jwtProvider.passwordEncode(form.getPassword()))
         .name(form.getName())
         .phone(form.getPhone())
         .major(form.getMajor())
+        .role(Role.ROLE_DOCTOR)
         .build();
 
-    doctor.setPassword(securityManager.passwordEncode(form.getPassword()));
-    doctor.setRole("ROLE_DOCTOR");
     return DoctorResponse.fromEntity(doctorRepository.save(doctor));
   }
 
   @Transactional
   public String doctorSignIn(SignInForm form) {
+
     Doctor doctor = doctorRepository.findByLoginId(form.getLoginId())
         .orElseThrow(() -> new CustomException(ErrorCode.ID_PASSWORD_INVALID));
 
-    if (!securityManager.checkPassword(form.getPassword(), doctor.getPassword())) {
+    //비밀번호 체크
+    if (!jwtProvider.checkPassword(form.getPassword(), doctor.getPassword())) {
       throw new CustomException(ErrorCode.ID_PASSWORD_INVALID);
     }
 
-    String token = securityManager.createToken(form.getLoginId());
+    //Jwt 토큰 반환
+    String token = jwtProvider.createToken(form.getLoginId());
     return "doctor " + token;
   }
 
   @Transactional
   public PatientResponse patientSignUp(PatientForm form) {
+
     if (patientRepository.existsByLoginId(form.getLoginId())) {
       throw new CustomException(ErrorCode.ID_EXIST);
     }
 
     Patient patient = Patient.builder()
         .loginId(form.getLoginId())
+        .password(jwtProvider.passwordEncode(form.getPassword()))
         .name(form.getName())
         .sex(form.getSex())
         .age(form.getAge())
         .phone(form.getPhone())
         .address(form.getAddress())
+        .role(Role.ROLE_PATIENT)
         .build();
 
-    patient.setPassword(securityManager.passwordEncode(form.getPassword()));
-    patient.setRole("ROLE_PATIENT");
     return PatientResponse.fromEntity(patientRepository.save(patient));
   }
 
   @Transactional
   public String patientSignIn(SignInForm form) {
+
     Patient patient = patientRepository.findByLoginId(form.getLoginId())
         .orElseThrow(() -> new CustomException(ErrorCode.ID_PASSWORD_INVALID));
 
-    if (!securityManager.checkPassword(form.getPassword(), patient.getPassword())) {
+    if (!jwtProvider.checkPassword(form.getPassword(), patient.getPassword())) {
       throw new CustomException(ErrorCode.ID_PASSWORD_INVALID);
     }
 
-    String token = securityManager.createToken(form.getLoginId());
+    String token = jwtProvider.createToken(form.getLoginId());
     return "patient " + token;
   }
 }
